77d4440a7c8712a6ffbf88bd4de6277a0bb73250
bfpead1 <blake.pead@gmail.com>
2026-02-19 19:52:58 -0700

    Updates to make mission7 work

--- Mission06_Pead/Controllers/MoviesController.cs
+++ Mission06_Pead/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Mission06_Pead.Data;
 using Mission06_Pead.Models;
 
@@ -15,23 +16,111 @@ namespace Mission06_Pead.Controllers
         }
 
         [HttpGet]
+        public IActionResult Index()
+        {
+            var movies = _context.Movies
+                .Include(m => m.Category)
+                .OrderBy(m => m.Title)
+                .ToList();
+
+            return View(movies);
+        }
+
+        [HttpGet]
         public IActionResult Add()
         {
-            ViewBag.Categories = new SelectList(_context.Categories.OrderBy(c => c.Name), "CategoryId", "Name");
+            PopulateCategories();
             return View(new Movie());
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Add(Movie movie)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                _context.Movies.Add(movie);
-                _context.SaveChanges();
-                return View("Confirmation", movie);
+                PopulateCategories(movie.CategoryId);
+                return View(movie);
             }
-            ViewBag.Categories = new SelectList(_context.Categories.OrderBy(c => c.Name), "CategoryId", "Name", movie.CategoryId);
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(m => m.MovieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            PopulateCategories(movie.CategoryId);
             return View(movie);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Movie movie)
+        {
+            if (id != movie.MovieId)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                PopulateCategories(movie.CategoryId);
+                return View(movie);
+            }
+
+            _context.Update(movie);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var movie = _context.Movies
+                .Include(m => m.Category)
+                .FirstOrDefault(m => m.MovieId == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var movie = _context.Movies.FirstOrDefault(m => m.MovieId == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private void PopulateCategories(int? selectedCategoryId = null)
+        {
+            ViewBag.Categories = new SelectList(
+                _context.Categories.OrderBy(c => c.CategoryName),
+                "CategoryId",
+                "CategoryName",
+                selectedCategoryId);
         }
     }
 }
